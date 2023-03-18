73727ed25a3a89fc233314c9cbd1082ec5e2df89
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2023-03-18 09:40:57 +0700

    update coordinate and movement history

---
+++ src/Solver/Coordinate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mrKrrabs.Solver
+{
+    public class Coordinate: Tuple<int, int>
+    {
+        public Coordinate(int x, int y) : base(x, y) { }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            
+            var that = obj as Coordinate;
+
+            if (that == null) return false;
+
+
+            if (that.Item1 == this.Item1 && that.Item2 == this.Item2) return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Item1, Item2).GetHashCode();
+        }
+    }
+}
--- src/Solver/MovementHistory.cs
+++ src/Solver/MovementHistory.cs
@@ -15,76 +15,26 @@ namespace mrKrrabs.Solver
     }
     public class MovementHistory
     {
-        int x;
-        int y;
-        readonly int startX;
-        readonly int startY;
-        List<Movement> movements = new List<Movement>();
-        List<Tuple<int, int>> movementCoordinates = new List<Tuple<int, int>>();
-        List<Movement> routes = new List<Movement>();
-        List<Tuple<int, int>> routeCoordinates = new List<Tuple<int, int>>();
+        List<Coordinate> movements = new();
+        List<Coordinate> routes = new();
 
-        public MovementHistory(int startX, int startY)
+        public MovementHistory()
         {
-            this.startX = startX;
-            this.startY = startY;
-            this.x = startX;
-            this.y = startY;
         }
 
-        public void Move(Movement movement)
+        public void Move(Coordinate movement)
         {
-            switch(movement)
-            {
-                case Movement.UP:
-                    this.y--;
-                    break;
-                case Movement.DOWN:
-                    this.y--;
-                    break;
-                case Movement.LEFT:
-                    this.x--;
-                    break;
-                case Movement.RIGHT:
-                    this.x++;
-                    break;
-            }
-
             this.movements.Add(movement);
-            this.movementCoordinates.Add(new Tuple<int,int>(this.x, this.y));
         }
 
-        public void SetRoute(List<Movement> routes)
+        public void SetRoute(List<Coordinate> routes)
         {
             this.routes = routes;
-            this.x = this.startX; 
-            this.y = this.startY;
-
-            foreach(var movement in this.routes)
-            {
-                switch (movement)
-                {
-                    case Movement.UP:
-                        this.y--;
-                        break;
-                    case Movement.DOWN:
-                        this.y--;
-                        break;
-                    case Movement.LEFT:
-                        this.x--;
-                        break;
-                    case Movement.RIGHT:
-                        this.x++;
-                        break;
-                }
-
-                this.movementCoordinates.Add(new Tuple<int, int>(this.x, this.y));
-            }
         }
 
-        public List<Tuple<int,int>> RouteCoordinates { get => routeCoordinates; }
-        public List<Tuple<int, int>> MovementCoordinates { get => movementCoordinates; }
+        public List<Coordinate> Routes { get => routes; }
+        public List<Coordinate> Movements { get => movements; }
 
-        public int StepCount { get => movementCoordinates.Count; }
+        public int StepCount { get => movements.Count; }
     }
 }
